296c4798561ef467c353c940fd5329204a0b9d6f
Bryan-Anthony <bryan.amoraes@sptech.school>
2025-09-01 16:43:39 -0300

    alterando consulta

--- app/consulta.py
+++ app/consulta.py
@@ -5,9 +5,12 @@ hora = True
 cpu = True
 ram = True
 disco = True
+macAddress = True
 
-def create_columns(hora, cpu, ram, disco):
+def create_columns(macAddress, hora, cpu, ram, disco):
     fcolumns = []
+    if macAddress : fcolumns.append('mac_address')
+    else : fcolumns.append('null')
     if hora : fcolumns.append('dt_registro') 
     else : fcolumns.append('null')
     if cpu : fcolumns.append('cpu_porcentagem')
@@ -19,21 +22,24 @@ def create_columns(hora, cpu, ram, disco):
     return fcolumns
 
 def exibir_consulta():
-    columns = create_columns(hora, cpu, ram, disco)
+    columns = create_columns(macAddress, hora, cpu, ram, disco)
     query = f"SELECT {', '.join(columns)} FROM captura_hardware_usage ORDER BY 1 DESC LIMIT 10;"
     resultado = mysql.executar(query)
     tabela = "~ RESULTADO DA QUERY ~\n"
+    if macAddress : tabela += "||    MAC Address   "
     if hora : tabela += "||     Timestamp       "
     if cpu : tabela += "|| Uso de CPU "
     if ram : tabela += "|| Uso de RAM "
     if disco : tabela += "|| Uso de Disco "
     tabela += "||\n"
+    
     for linha in resultado:
         tabela += "\n"
-        if hora : tabela += f"|| {linha[0]} "
-        if cpu : tabela += f"||   {linha[1]:04}%    "
-        if ram : tabela += f"||   {linha[2]:04}%    "
-        if disco : tabela += f"||    {linha[3]:04}%     "
+        if macAddress : tabela += f"|| {linha[0]}"
+        if hora : tabela += f"|| {linha[1]} "
+        if cpu : tabela += f"||   {linha[2]:04}%    "
+        if ram : tabela += f"||   {linha[3]:04}%    "
+        if disco : tabela += f"||    {linha[4]:04}%     "
         tabela += "||\n"
     print(tabela)
 
@@ -42,27 +48,30 @@ while True:
     exibir_consulta()
 
     print("""
-  =========== MENU - Digite um número ===========
-  1 - timestamp on/off  |  4 - uso de disco on/off                      |  7 - fechar programa
-  2 - uso de cpu on/off |  5 - deletar últimos 5 registros              |  
-  3 - uso de ram on/off |  6 - atualizar timestamp últimos 3 registros  |  
+                              =========== MENU - Digite um número ===========
+  1 - MAC Address on/off |  4 - uso de RAM on/off            |  7 - atualizar timestamp últimos 3 registros
+  2 - timestamp on/off   |  5 - uso de Disco on/off          |  8 - fechar programa
+  3 - uso de CPU on/off  |  6 - deletar últimos 5 registros  |  
 """)
     comando = input("digite seu comando~ ")
     os.system('cls')
+    
     match comando:
         case "1":
-            hora = not hora
+            macAddress = not macAddress
         case "2":
-            cpu = not cpu
+            hora = not hora
         case "3":
-            ram = not ram
+            cpu = not cpu
         case "4":
-            disco = not disco
+            ram = not ram
         case "5":
-            mysql.executar("DELETE FROM captura_hardware_usage WHERE id IN (SELECT * FROM (SELECT id FROM captura_hardware_usage ORDER BY dt_registro DESC, id DESC LIMIT 5) AS temp);")
+            disco = not disco
         case "6":
-            mysql.executar("UPDATE captura_hardware_usage SET dt_registro = current_timestamp WHERE id IN (SELECT * FROM (SELECT id FROM captura_hardware_usage ORDER BY dt_registro DESC, id DESC LIMIT 3) AS temp);")
+            mysql.executar("DELETE FROM captura_hardware_usage WHERE id IN (SELECT * FROM (SELECT id FROM captura_hardware_usage ORDER BY dt_registro DESC, id DESC LIMIT 5) AS temp);")
         case "7":
+            mysql.executar("UPDATE captura_hardware_usage SET dt_registro = current_timestamp WHERE id IN (SELECT * FROM (SELECT id FROM captura_hardware_usage ORDER BY dt_registro DESC, id DESC LIMIT 3) AS temp);")
+        case "8":
             print("\nAté a próxima! Desligando programa...")
             break
         case _:
