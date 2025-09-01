bb8e9cf24fb673301254dd03d48b16c4603975b9
MariaLuizaCostaGoes <maria.goes@sptech.school>
2025-09-01 16:29:45 -0300

    Captura do mac address

--- app/captura.py
+++ app/captura.py
@@ -1,28 +1,33 @@
 import psutil as p, datetime, time, os
 from utils import mysql
 
-while True :
-    os.system('cls')
+
+for i in range(20):
+    # os.system('cls')
     uso_cpu = p.cpu_percent(interval=None, percpu=False)
     uso_ram = p.virtual_memory().percent
     uso_disco = p.disk_usage("C://").percent
     horario_medicao = datetime.datetime.now().strftime("%H:%M:%S")
+    mac_address = p.net_if_addrs().get('Wi-Fi')[0][1]
 
-    mysql.executar(f"INSERT INTO captura_hardware_usage (cpu_porcentagem, ram_porcentagem, disco_porcentagem) VALUES ({uso_cpu}, {uso_ram}, {uso_disco})")
+    mysql.executar(f"INSERT INTO captura_hardware_usage (cpu_porcentagem, ram_porcentagem, disco_porcentagem, mac_address) VALUES ({uso_cpu}, {uso_ram}, {uso_disco}, '{mac_address}')")
 
     print(f"""
     #################################################
     #        Dados inseridos com sucesso!           #
     #################################################
 
-        ################################
-        #   Uso de CPU   #    {uso_cpu:04}%    #
-        #------------------------------#
-        #   Uso de RAM   #    {uso_ram:04}%    #
-        #------------------------------#
-        #  Uso de Disco  #    {uso_disco:04}%    #
-        #------------------------------#
-        #            Horário: {horario_medicao:04} #
-        ################################
+        #########################################
+        #   Uso de CPU   #        {uso_cpu:04}%         #
+        #---------------------------------------#
+        #   Uso de RAM   #        {uso_ram:04}%         #
+        #---------------------------------------#
+        #  Uso de Disco  #        {uso_disco:04}%         #
+        #---------------------------------------#
+        #  MAC Address   #   {mac_address}  #
+        #---------------------------------------#
+        #           Horário: {horario_medicao:04}           #
+        #########################################
     """)
-    time.sleep(10)
+    time.sleep(2)
+
