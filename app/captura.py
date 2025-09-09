eb9a2763b0ace0d8f7ee2817395bd9d7da271fc1
MariaLuizaCostaGoes <maria.goes@sptech.school>
2025-09-09 17:24:42 -0300

    Alteração no script MySQL para a captura da data e hora

--- app/captura.py
+++ app/captura.py
@@ -8,15 +8,21 @@ time.sleep(2)
 
 for i in range(20):
     os.system('cls')
-    mac_address = p.net_if_addrs().get('Wi-Fi')[0][1]
+
+    if p.LINUX :
+        temperatura_cpu = p.sensors_temperatures(fahrenheit=False).get('coretemp')[0][1]
+        mac_address = p.net_if_addrs().get('wlan0')[2][1]
+
+    if p.WINDOWS :
+        temperatura_cpu = "null"
+        mac_address = p.net_if_addrs().get('Wi-Fi')[0][1]
+   
     uso_cpu = p.cpu_percent(interval=None, percpu=False)
     uso_ram = p.virtual_memory().percent
     uso_disco = p.disk_usage("C://").percent
-    pkts_rec_agora = p.net_io_counters(pernic=False, nowrap=True).packets_recv 
+    pkts_rec_agora = p.net_io_counters(pernic=False, nowrap=True).packets_recv
     pkts_env_agora = p.net_io_counters(pernic=False, nowrap=True).packets_sent 
-    horario_medicao = datetime.datetime.now().strftime("%H:%M:%S")
-
-    #temp_cpu = p.sensors_temperatures(fahrenheit=False).get('coretemp')[0][1]
+    horario_medicao = datetime.datetime.now()
 
     pkts_rec_por_seg = (pkts_rec_agora - pkts_rec_antes) / 2
     pkts_env_por_seg = (pkts_env_agora - pkts_env_antes) / 2
@@ -24,7 +30,7 @@ for i in range(20):
     pkts_rec_antes = pkts_rec_agora
     pkts_env_antes = pkts_env_agora
 
-    mysql.executar(f"INSERT INTO leitura (uso_medioCPU, uso_memoria, uso_disco, fk_mac_address, throughput_recebido, throughput_enviado) VALUES ({uso_cpu}, {uso_ram}, {uso_disco}, '{mac_address}', {pkts_rec_por_seg}, {pkts_env_por_seg})")
+    mysql.executar(f"INSERT INTO leitura (uso_medioCPU, uso_memoria, uso_disco, fk_mac_address, throughput_recebido, throughput_enviado, temperatura_cpu, data_hora) VALUES ({uso_cpu}, {uso_ram}, {uso_disco}, '{mac_address.replace("-", "").replace(":", "")}', {pkts_rec_por_seg}, {pkts_env_por_seg}, {temperatura_cpu}, '{horario_medicao}')")
 
     print(f"""
     ###################################################################
@@ -36,15 +42,17 @@ for i in range(20):
         #----------------------------------------------------------#
         #   Uso de CPU                     #        {uso_cpu:04}%          #
         #----------------------------------------------------------#
+        #   Temperatura CPU                #         {temperatura_cpu}          #
+        #----------------------------------------------------------#
         #   Uso de RAM                     #        {uso_ram:04}%          #
         #----------------------------------------------------------#
         #   Uso de Disco                   #        {uso_disco:04}%          #
         #----------------------------------------------------------#
-        #   Pacotes Recebidos por segundo  #        {pkts_rec_por_seg }          #
+        #   Pacotes Recebidos por segundo  #        {pkts_rec_por_seg:05}          #
         #----------------------------------------------------------#
-        #   Pacotes Enviados por segundo   #        {pkts_env_por_seg}            #
+        #   Pacotes Enviados por segundo   #        {pkts_env_por_seg:05}          #
         #----------------------------------------------------------#
-        #                     Horário: {horario_medicao:04}                    #
+        #                     Horário: {horario_medicao.strftime("%H:%M:%S"):04}                    #
         ############################################################
     """)
     time.sleep(2)
--- app/consulta.py
+++ app/consulta.py
@@ -8,28 +8,31 @@ disco = True
 macAddress = True
 pkts_rec = True
 pkts_env = True
+temp_cpu = True
 
-def create_columns(macAddress, hora, cpu, ram, disco):
+def create_columns(macAddress, hora, cpu, ram, disco, pkts_rec, pkts_env, temp_cpu):
     fcolumns = []
-    if macAddress : fcolumns.append('mac_address')
+    if macAddress : fcolumns.append('fk_mac_address')
     else : fcolumns.append('null')
-    if hora : fcolumns.append('horario_medicao') 
+    if hora : fcolumns.append('data_hora') 
     else : fcolumns.append('null')
-    if cpu : fcolumns.append('uso_cpu')
+    if cpu : fcolumns.append('uso_medioCPU')
     else : fcolumns.append('null')
-    if ram : fcolumns.append('uso_ram')
+    if ram : fcolumns.append('uso_memoria')
     else : fcolumns.append('null')
     if disco : fcolumns.append('uso_disco')
     else : fcolumns.append('null')
-    if pkts_rec : fcolumns.append('pkts_rec_por_seg')
+    if pkts_rec : fcolumns.append('throughput_recebido')
     else : fcolumns.append('null')
-    if pkts_env : fcolumns.append('pkts_env_por_seg')
+    if pkts_env : fcolumns.append('throughput_enviado')
+    else : fcolumns.append('null')
+    if temp_cpu : fcolumns.append('temperatura_cpu')
     else : fcolumns.append('null')
     return fcolumns
 
 def exibir_consulta():
-    columns = create_columns(macAddress, hora, cpu, ram, disco)
-    query = f"SELECT {', '.join(columns)} FROM captura_hardware_usage ORDER BY 1 DESC LIMIT 10;"
+    columns = create_columns(macAddress, hora, cpu, ram, disco, pkts_rec, pkts_env, temp_cpu)
+    query = f"SELECT {', '.join(columns)} FROM leitura ORDER BY 1 DESC LIMIT 10;"
     resultado = mysql.executar(query)
     tabela = "~ RESULTADO DA QUERY ~\n"
     if macAddress : tabela += "||    MAC Address   "
@@ -37,6 +40,9 @@ def exibir_consulta():
     if cpu : tabela += "|| Uso de CPU "
     if ram : tabela += "|| Uso de RAM "
     if disco : tabela += "|| Uso de Disco "
+    if pkts_rec : tabela += "|| Pacotes Recebidos "
+    if pkts_env : tabela += "|| Pacotes Enviados "
+    if temp_cpu : tabela += "|| Temperatura da CPU "
     tabela += "||\n"
     
     for linha in resultado:
@@ -46,6 +52,9 @@ def exibir_consulta():
         if cpu : tabela += f"||   {linha[2]:04}%    "
         if ram : tabela += f"||   {linha[3]:04}%    "
         if disco : tabela += f"||    {linha[4]:04}%     "
+        if pkts_rec : tabela += f"|| {linha[5]}"
+        if pkts_env : tabela += f"|| {linha[6]}"
+        if temp_cpu : tabela += f"|| {linha[7]}"
         tabela += "||\n"
     print(tabela)
 
@@ -54,10 +63,10 @@ while True:
     exibir_consulta()
 
     print("""
-                              =========== MENU - Digite um número ===========
-  1 - MAC Address on/off |  4 - uso de RAM on/off            |  7 - atualizar timestamp últimos 3 registros
-  2 - timestamp on/off   |  5 - uso de Disco on/off          |  8 - fechar programa
-  3 - uso de CPU on/off  |  6 - deletar últimos 5 registros  |  9 - 
+                                            =========== MENU - Digite um número ===========
+  1 - MAC Address on/off    |  4 - uso de RAM on/off            |  7 - pacotes enviados on/off        | 10 - atualizar timestamp últimos 3 registros
+  2 - timestamp on/off      |  5 - uso de Disco on/off          |  8 - temperatura CUP                | 11 - fechar programa
+  3 - uso de CPU on/off     |  6 - pacotes recebidos on/off     |  9 - deletar últimos 5 registros    |
 """)
     comando = input("digite seu comando~ ")
     os.system('cls')
@@ -74,10 +83,14 @@ while True:
         case "5":
             disco = not disco
         case "6":
-            mysql.executar("DELETE FROM captura_hardware_usage WHERE id IN (SELECT * FROM (SELECT id FROM captura_hardware_usage ORDER BY dt_registro DESC, id DESC LIMIT 5) AS temp);")
+            pkts_rec = not pkts_rec
         case "7":
-            mysql.executar("UPDATE captura_hardware_usage SET dt_registro = current_timestamp WHERE id IN (SELECT * FROM (SELECT id FROM captura_hardware_usage ORDER BY dt_registro DESC, id DESC LIMIT 3) AS temp);")
+            pkts_env = not pkts_env
         case "8":
+            mysql.executar("DELETE FROM leitura WHERE id IN (SELECT * FROM (SELECT id FROM leitura ORDER BY dt_registro DESC, id DESC LIMIT 5) AS temp);")
+        case "9":
+            mysql.executar("UPDATE leitura SET dt_registro = current_timestamp WHERE id IN (SELECT * FROM (SELECT id FROM captura_hardware_usage ORDER BY dt_registro DESC, id DESC LIMIT 3) AS temp);")
+        case "10":
             print("\nAté a próxima! Desligando programa...")
             break
         case _:
