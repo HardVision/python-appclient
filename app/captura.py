0769a4bf3cf6043bcfe25d7b4a034eabaf9fb48f
JoaoVtC <joao.varaujo@spetch.school>
2025-09-29 15:33:40 -0300

    Adição da metrica nas Fks

--- app/captura.py
+++ app/captura.py
@@ -1,58 +1,96 @@
-import psutil as p, datetime, time, os
-from utils import mysql
+import psutil as p
+from mysql.connector import connect, Error
+import dotenv as d
+from os import getenv
+from time import sleep
 
-pkts_rec_antes = p.net_io_counters(pernic=False, nowrap=True).packets_recv 
-pkts_env_antes = p.net_io_counters(pernic=False, nowrap=True).packets_sent 
-print("Iniciando...")
-time.sleep(2)
+d.load_dotenv()
 
-for i in range(20):
-    os.system('cls')
 
-    if p.LINUX :
-        temperatura_cpu = p.sensors_temperatures(fahrenheit=False).get('coretemp')[0][1]
-        mac_address = p.net_if_addrs().get('wlan0')[2][1]
+def conectar_server():
+    config = {
+        'user': getenv("USER_DB"),
+        'password': getenv("PASSWORD_DB"),
+        'host': getenv("HOST_DB"),
+        'database': getenv("DATABASE_DB"),
+    }
 
-    if p.WINDOWS :
-        temperatura_cpu = "null"
-        mac_address = p.net_if_addrs().get('Wi-Fi')[0][1]
-   
-    uso_cpu = p.cpu_percent(interval=None, percpu=False)
-    uso_ram = p.virtual_memory().percent
-    uso_disco = p.disk_usage("C://").percent
-    pkts_rec_agora = p.net_io_counters(pernic=False, nowrap=True).packets_recv
-    pkts_env_agora = p.net_io_counters(pernic=False, nowrap=True).packets_sent 
-    horario_medicao = datetime.datetime.now()
-
-    pkts_rec_por_seg = (pkts_rec_agora - pkts_rec_antes) / 2
-    pkts_env_por_seg = (pkts_env_agora - pkts_env_antes) / 2
+    try:
+        db = connect(**config)
+        if db.is_connected():
+            db_info = db.get_server_info()
+            print('Connected to MySQL server version -', db_info)
+        return db
     
-    pkts_rec_antes = pkts_rec_agora
-    pkts_env_antes = pkts_env_agora
+    except Error as e:
+        print('Error to connect with MySQL -', e)
+        return None
 
-    mysql.executar(f"INSERT INTO leitura (uso_medioCPU, uso_memoria, uso_disco, fk_mac_address, throughput_recebido, throughput_enviado, temperatura_cpu, data_hora) VALUES ({uso_cpu}, {uso_ram}, {uso_disco}, '{mac_address.replace("-", "").replace(":", "")}', {pkts_rec_por_seg}, {pkts_env_por_seg}, {temperatura_cpu}, '{horario_medicao}')")
 
-    print(f"""
-    ###################################################################
-    #                  Dados inseridos com sucesso!                   #
-    ###################################################################
+def identifica_fk(db, modelo, macAdress):
+    try:
+        macAdress = macAdress,
+        modelo = modelo,
+        with db.cursor() as cursor:
+            slctQueryMaq = "SELECT idMaquina FROM maquina WHERE macAddress = %s"
+            cursor.execute(slctQueryMaq, macAdress)
+            idMaquina = cursor.fetchone()  
 
-        ############################################################
-        #   MAC Address                    #   {mac_address}   #
-        #----------------------------------------------------------#
-        #   Uso de CPU                     #        {uso_cpu:04}%          #
-        #----------------------------------------------------------#
-        #   Temperatura CPU                #         {temperatura_cpu}          #
-        #----------------------------------------------------------#
-        #   Uso de RAM                     #        {uso_ram:04}%          #
-        #----------------------------------------------------------#
-        #   Uso de Disco                   #        {uso_disco:04}%          #
-        #----------------------------------------------------------#
-        #   Pacotes Recebidos por segundo  #        {pkts_rec_por_seg:05}          #
-        #----------------------------------------------------------#
-        #   Pacotes Enviados por segundo   #        {pkts_env_por_seg:05}          #
-        #----------------------------------------------------------#
-        #                     Horário: {horario_medicao.strftime("%H:%M:%S"):04}                    #
-        ############################################################
-    """)
-    time.sleep(2)
+            slctQueryComp = "SELECT idComponente FROM Componente WHERE modelo = %s"
+            cursor.execute(slctQueryComp, modelo)
+            idComponente = cursor.fetchall()  
+
+            slctQueryComp = "SELECT fkMetrica FROM Componente WHERE modelo = %s"
+            cursor.execute(slctQueryComp, modelo)
+            idMetrica = cursor.fetchone()  
+
+            return {"idMaquina":idMaquina, 
+                    "idComponente": idComponente,
+                    "idMetrica": idMetrica}
+        
+    except Error as e:
+        print('Error ao selecionar no MySQL -', e , " - identifica-fk")
+
+
+def inserir_porcentagem(porc, db, idMaquina, idComponente, idMetrica):
+    print("Fks: ", idMaquina, idComponente, idMetrica)
+    print("Valor a ser adicionado: ", porc)
+    try:
+        with db.cursor() as cursor:
+            if idMaquina and idComponente:
+                for i in range(0, len(idComponente)):
+                    query = """
+                        INSERT INTO LogMonitoramento (fkComponente, fkMaquina, fkMetrica, valor, descricao)
+                        VALUES (%s, %s, %s, %s, %s)
+                    """
+                    values = (idComponente[i][0], idMaquina[0], idMetrica[0], porc[i], "Valor adicionado")
+                    cursor.execute(query, values)
+                    db.commit()
+                    print(cursor.rowcount, "registro inserido na tabela Monitoramento")
+            else:
+                print("Máquina ou componente não encontrados no banco de dados.")
+
+    except Error as e:
+        print('Error ao inserir no MySQL -', e)
+
+
+
+
+db = conectar_server()
+fkCpu = identifica_fk(db, "Intel Xeon", "00:1A:2B:3C:4D:5E")
+fkRam = identifica_fk(db, "DDR4", "00:1A:2B:3C:4D:5E")
+fkDisc = identifica_fk(db, "Seagate", "00:1A:2B:3C:4D:5E")
+print(fkRam)
+
+while True:
+    ram_porc = p.virtual_memory().percent,
+    disc_porc = p.disk_usage("/").percent,
+    cpu_porc = p.cpu_percent(interval=1, percpu=True)
+    if db:
+        # Chamada para salvar no banco
+        inserir_porcentagem(cpu_porc, db, fkCpu["idMaquina"], fkCpu["idComponente"], fkCpu["idMetrica"])
+        inserir_porcentagem(ram_porc, db, fkRam["idMaquina"], fkRam["idComponente"], fkRam["idMetrica"])
+        inserir_porcentagem(disc_porc, db, fkDisc["idMaquina"], fkDisc["idComponente"], fkDisc["idMetrica"])
+
+      
+    sleep(2)
--- app/consulta.py
+++ app/consulta.py
@@ -1,97 +1,84 @@
-from utils import mysql
-import os
+from mysql.connector import connect, Error
+from os import getenv
+import dotenv as d
+from tabulate import tabulate
 
-hora = True
-cpu = True
-ram = True
-disco = True
-macAddress = True
-pkts_rec = True
-pkts_env = True
-temp_cpu = True
+d.load_dotenv()
 
-def create_columns(macAddress, hora, cpu, ram, disco, pkts_rec, pkts_env, temp_cpu):
-    fcolumns = []
-    if macAddress : fcolumns.append('fk_mac_address')
-    else : fcolumns.append('null')
-    if hora : fcolumns.append('data_hora') 
-    else : fcolumns.append('null')
-    if cpu : fcolumns.append('uso_medioCPU')
-    else : fcolumns.append('null')
-    if ram : fcolumns.append('uso_memoria')
-    else : fcolumns.append('null')
-    if disco : fcolumns.append('uso_disco')
-    else : fcolumns.append('null')
-    if pkts_rec : fcolumns.append('throughput_recebido')
-    else : fcolumns.append('null')
-    if pkts_env : fcolumns.append('throughput_enviado')
-    else : fcolumns.append('null')
-    if temp_cpu : fcolumns.append('temperatura_cpu')
-    else : fcolumns.append('null')
-    return fcolumns
 
-def exibir_consulta():
-    columns = create_columns(macAddress, hora, cpu, ram, disco, pkts_rec, pkts_env, temp_cpu)
-    query = f"SELECT {', '.join(columns)} FROM leitura ORDER BY 1 DESC LIMIT 10;"
-    resultado = mysql.executar(query)
-    tabela = "~ RESULTADO DA QUERY ~\n"
-    if macAddress : tabela += "||    MAC Address   "
-    if hora : tabela += "||     Timestamp       "
-    if cpu : tabela += "|| Uso de CPU "
-    if ram : tabela += "|| Uso de RAM "
-    if disco : tabela += "|| Uso de Disco "
-    if pkts_rec : tabela += "|| Pacotes Recebidos "
-    if pkts_env : tabela += "|| Pacotes Enviados "
-    if temp_cpu : tabela += "|| Temperatura da CPU "
-    tabela += "||\n"
+def conectar_server():
+    config = {
+    'user': getenv("USER_SELECT_DB"),
+    'password': getenv("PASSWORD_DB"),
+    'host': getenv("HOST_DB"),
+    'database': getenv("DATABASE_DB"),
+    'port': getenv("PORT_DB")
+    }
+   
     
-    for linha in resultado:
-        tabela += "\n"
-        if macAddress : tabela += f"|| {linha[0]}"
-        if hora : tabela += f"|| {linha[1]} "
-        if cpu : tabela += f"||   {linha[2]:04}%    "
-        if ram : tabela += f"||   {linha[3]:04}%    "
-        if disco : tabela += f"||    {linha[4]:04}%     "
-        if pkts_rec : tabela += f"|| {linha[5]}"
-        if pkts_env : tabela += f"|| {linha[6]}"
-        if temp_cpu : tabela += f"|| {linha[7]}"
-        tabela += "||\n"
-    print(tabela)
-
-
-while True:
-    exibir_consulta()
-
-    print("""
-                                            =========== MENU - Digite um número ===========
-  1 - MAC Address on/off    |  4 - uso de RAM on/off            |  7 - pacotes enviados on/off        | 10 - atualizar timestamp últimos 3 registros
-  2 - timestamp on/off      |  5 - uso de Disco on/off          |  8 - temperatura CUP                | 11 - fechar programa
-  3 - uso de CPU on/off     |  6 - pacotes recebidos on/off     |  9 - deletar últimos 5 registros    |
-""")
-    comando = input("digite seu comando~ ")
-    os.system('cls')
+    try:
+        db = connect(**config)
+        if db.is_connected():
+            db_info = db.get_server_info() 
+        print('Connected to MySQL server version -', db_info)
+        return db
     
-    match comando:
-        case "1":
-            macAddress = not macAddress
-        case "2":
-            hora = not hora
-        case "3":
-            cpu = not cpu
-        case "4":
-            ram = not ram
-        case "5":
-            disco = not disco
-        case "6":
-            pkts_rec = not pkts_rec
-        case "7":
-            pkts_env = not pkts_env
-        case "8":
-            mysql.executar("DELETE FROM leitura WHERE id IN (SELECT * FROM (SELECT id FROM leitura ORDER BY dt_registro DESC, id DESC LIMIT 5) AS temp);")
-        case "9":
-            mysql.executar("UPDATE leitura SET dt_registro = current_timestamp WHERE id IN (SELECT * FROM (SELECT id FROM captura_hardware_usage ORDER BY dt_registro DESC, id DESC LIMIT 3) AS temp);")
-        case "10":
-            print("\nAté a próxima! Desligando programa...")
+    except Error as e:
+        print('Error to connect with MySQL -', e)
+        return None
+
+def exibir_maquinas(db):
+    cursor = db.cursor() 
+    cursor.execute("SELECT idMaquina, macAdress FROM Maquina")
+    headers = ["Maquina", "Mac Adress"]
+    resultados = cursor.fetchall()
+    print("\n--- Máquinas ---")
+    print(tabulate(resultados, headers, tablefmt="fancy_grid"))
+    cursor.close()
+
+def exibir_componentes(db):
+    cursor = db.cursor()
+    cursor.execute("SELECT idComponente, tipo, modelo FROM Componente")
+    headers = ["Componente", "Tipo", "Modelo"]
+    resultados = cursor.fetchall()
+    print("\n--- Componentes ---")
+    print(tabulate(resultados, headers, tablefmt="fancy_grid"))
+    cursor.close()
+
+def exibir_monitoramento(db):
+    cursor = db.cursor()
+    cursor.execute("SELECT fkMaquina, fkComponente, macAdress, tipo, modelo, valor, dtHora FROM LogMonitoramento join Componente on fkComponente = idComponente join Maquina on fkMaquina = idMaquina order by idMonitoramento desc LIMIT 10")
+    headers = ["Máquina", "Componente", "Mac Adress", "Tipo", "Modelo", "Valor %","Data de captura"]
+    resultados = cursor.fetchall()
+    print("\n--- Monitoramento (últimos registros) ---")
+    print(tabulate(resultados, headers, tablefmt="fancy_grid"))
+    cursor.close()
+def menu():
+    db = conectar_server()
+    
+    if not db:
+        return
+    
+    while True:
+        print("\n=== MENU ===")
+        print("1 - Listar Máquinas")
+        print("2 - Listar Componentes")
+        print("3 - Mostrar Monitoramento")
+        print("0 - Sair")
+        
+        opcao = input("Escolha uma opção: ")
+
+        if opcao == "1":
+            exibir_maquinas(db)
+        elif opcao == "2":
+            exibir_componentes(db)
+        elif opcao == "3":
+            exibir_monitoramento(db)
+        elif opcao == "0":
+            print("Operação Finalizada!")
             break
-        case _:
-            continue
+        else:
+            print("Opção inválida!")
+
+    db.close()
+menu()
